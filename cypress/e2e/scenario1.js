e48c7f1e5ad34697670216e6b4c55298d1214e7d
Subhadip Das <88592485+engineersubhadip@users.noreply.github.com>
2026-07-08 18:12:47 +0530

    Text change (#2)
    * How to verify text change
    
    * How to validate text change, when the before and after text is random and unknown
    
    * How to capture text change appraoch 2

--- cypress/e2e/scenario1.js
+++ cypress/e2e/scenario1.js
@@ -1,12 +1,31 @@
 /// <reference types="cypress" />
 
 describe("Test Suite", () => {
+<<<<<<< HEAD
+  it("Verify text change : Approach 1", () => {
+    cy.visit("/index1.html");
+    cy.get("#output")
+      .invoke("text")
+      .then((currentText) => {
+        cy.get("#change").click();
+        cy.get("#output").invoke("text").should("not.equal", currentText);
+      });
+  });
+
+  it.only("Verify text change : Approach 2", () => {
+    cy.visit("/index1.html");
+    cy.get("#output").invoke("text").as("oldText", { type: "static" });
+    cy.get("#change").click();
+    cy.get("@oldText").then((oldText) => {
+      cy.get("#output").invoke("text").should("not.equal", oldText);
+=======
   it("Verify text change", () => {
     cy.visit("/index1.html");
     cy.get("#output").invoke("text").as("originalText", { type: "static" });
     cy.get("#change").click();
     cy.get("@originalText").then((text) => {
       cy.get("#output").invoke("text").should("not.equal", text);
+>>>>>>> 0eecfceff0ddc03414a118f481fe3fdb46bddda8
     });
   });
 });
