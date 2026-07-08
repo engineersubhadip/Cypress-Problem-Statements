861ec8eaabbe3807e911ed2c054cc97c78fadc44
unknown <subhadipdas911@gmail.com>
2026-07-08 18:16:38 +0530

    Cleaned code

--- cypress/e2e/scenario1.js
+++ cypress/e2e/scenario1.js
@@ -1,7 +1,6 @@
 /// <reference types="cypress" />
 
 describe("Test Suite", () => {
-<<<<<<< HEAD
   it("Verify text change : Approach 1", () => {
     cy.visit("/index1.html");
     cy.get("#output")
@@ -12,20 +11,12 @@ describe("Test Suite", () => {
       });
   });
 
-  it.only("Verify text change : Approach 2", () => {
+  it("Verify text change : Approach 2", () => {
     cy.visit("/index1.html");
     cy.get("#output").invoke("text").as("oldText", { type: "static" });
     cy.get("#change").click();
     cy.get("@oldText").then((oldText) => {
       cy.get("#output").invoke("text").should("not.equal", oldText);
-=======
-  it("Verify text change", () => {
-    cy.visit("/index1.html");
-    cy.get("#output").invoke("text").as("originalText", { type: "static" });
-    cy.get("#change").click();
-    cy.get("@originalText").then((text) => {
-      cy.get("#output").invoke("text").should("not.equal", text);
->>>>>>> 0eecfceff0ddc03414a118f481fe3fdb46bddda8
     });
   });
 });
