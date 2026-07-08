1d9ee894fa1bad70e1d1259330e1e1d95f08a52d
unknown <subhadipdas911@gmail.com>
2026-07-08 18:09:46 +0530

    How to capture text change appraoch 2

--- cypress/e2e/scenario1.js
+++ cypress/e2e/scenario1.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("Test Suite", () => {
-  it("Verify text change", () => {
+  it("Verify text change : Approach 1", () => {
     cy.visit("/index1.html");
     cy.get("#output")
       .invoke("text")
@@ -9,5 +9,14 @@ describe("Test Suite", () => {
         cy.get("#change").click();
         cy.get("#output").invoke("text").should("not.equal", currentText);
       });
+  });
+
+  it.only("Verify text change : Approach 2", () => {
+    cy.visit("/index1.html");
+    cy.get("#output").invoke("text").as("oldText", { type: "static" });
+    cy.get("#change").click();
+    cy.get("@oldText").then((oldText) => {
+      cy.get("#output").invoke("text").should("not.equal", oldText);
+    });
   });
 });
