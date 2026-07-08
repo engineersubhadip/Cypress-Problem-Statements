932590bce95ebc511b843096b4c5be82209578c0
unknown <subhadipdas911@gmail.com>
2026-07-08 17:57:34 +0530

    How to validate text change, when the before and after text is random and unknown

--- cypress/e2e/scenario1.js
+++ cypress/e2e/scenario1.js
@@ -3,10 +3,11 @@
 describe("Test Suite", () => {
   it("Verify text change", () => {
     cy.visit("/index1.html");
-    cy.get("#output").invoke("text").as("originalText", { type: "static" });
-    cy.get("#change").click();
-    cy.get("@originalText").then((text) => {
-      cy.get("#output").invoke("text").should("not.equal", text);
-    });
+    cy.get("#output")
+      .invoke("text")
+      .then((currentText) => {
+        cy.get("#change").click();
+        cy.get("#output").invoke("text").should("not.equal", currentText);
+      });
   });
 });
