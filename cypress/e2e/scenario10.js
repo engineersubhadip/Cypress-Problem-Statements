1ede81cefc1d63d5137adc40241b95fc3b2cf1ed
unknown <subhadipdas911@gmail.com>
2026-07-11 13:11:08 +0530

    how to capture row data in async fashio

---
+++ cypress/e2e/scenario10.js
@@ -0,0 +1,36 @@
+/// <reference types='cypress' />
+
+let finalArr = [];
+
+function captureAllRowDetails(currentCounter) {
+  if (currentCounter > 4) {
+    return;
+  }
+  cy.get("body tbody")
+    .should("exist")
+    .then(($parent) => {
+      const rowStatus = $parent.find("tr").length > 0;
+      if (rowStatus) {
+        cy.wrap($parent)
+          .find("tr")
+          .eq(currentCounter)
+          .should("be.visible")
+          .and(() => {})
+          .invoke("text")
+          .should("have.length.at.least", 1)
+          .then((capturedText) => {
+            finalArr.push(capturedText);
+          })
+          .then(() => captureAllRowDetails(currentCounter + 1));
+      } else {
+        cy.wait(200).then(() => captureAllRowDetails(currentCounter));
+      }
+    });
+}
+
+it("Capturing data in async fashion", () => {
+  cy.visit("/index13.html");
+  finalArr = [];
+  captureAllRowDetails(0);
+  cy.log(finalArr);
+});
