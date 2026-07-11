9f7d10b6b6584a88c3725dc5d819add8a3d3fe3f
unknown <subhadipdas911@gmail.com>
2026-07-11 15:08:13 +0530

    How tot traverse to parent from child, for multiple childres

--- cypress/e2e/scenario11.js
+++ cypress/e2e/scenario11.js
@@ -5,7 +5,12 @@ it("Capture li which has text has Advanced", () => {
   cy.contains("li", "Advanced!");
 });
 
-it.only("Capture the top level parents", () => {
+it("Capture the top level parents", () => {
   cy.visit("/index14.html");
   cy.get("li").find(".label").parents("li");
+});
+
+it.only("Capture li which has warning tags", () => {
+  cy.visit("/index15.html");
+  cy.get("li").find(".badge.warning").parents("li");
 });
