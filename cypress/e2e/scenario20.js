e3be4d5732511f43b61fc39960d24625883f6800
unknown <subhadipdas911@gmail.com>
2026-07-16 17:10:17 +0530

    Code change

--- cypress/e2e/scenario20.js
+++ cypress/e2e/scenario20.js
@@ -4,7 +4,6 @@ it("Find and click accordian with button : Approach 1", () => {
   cy.visit("/index22.html");
   //   cy.get("div.panel:has(button)").prev().click();
   cy.get("div.panel").each((currEle, index) => {
-    console.log("DEBUG -> ", index);
     const status = currEle.find("button").length > 0;
     if (status) {
       cy.wrap(currEle).prev().click();
@@ -15,7 +14,7 @@ it("Find and click accordian with button : Approach 1", () => {
 
 it("Find and click accordian with button : Approach 2", () => {
   cy.visit("/index22.html");
-  cy.get("div.panel").find("button").parent().prev().click();
+  cy.get("div.panel").find("button:contains('here')").parent().prev().click();
 });
 
 it.only("Find and click accordian with button : Approach 3 [Best Approach]", () => {
