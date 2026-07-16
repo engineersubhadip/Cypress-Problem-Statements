1ea4aebd1443dd6eafdae9188064ca37bd955bfb
unknown <subhadipdas911@gmail.com>
2026-07-16 18:45:29 +0530

    How to capture parent text, part 2

--- cypress/e2e/scenario23.js
+++ cypress/e2e/scenario23.js
@@ -1,11 +1,23 @@
 /// <reference types='cypress' />
 
-it("Capture the text of the parent", () => {
+it("Capture the text of the parent : Approach 1", () => {
   cy.visit("/index25.html");
   cy.get("#make")
     .should("be.visible")
     .then((parent) => {
       const targetText = parent[0].childNodes[0].textContent.trim();
       console.log(targetText);
+    });
+});
+
+it.only("Capture the text of the parent : Approach 2", () => {
+  cy.visit("/index25.html");
+  cy.get("#make")
+    .should("be.visible")
+    .then((parent) => {
+      let clonedParent = parent.clone();
+      clonedParent.children().remove();
+      const parentText = clonedParent.text().trim();
+      console.log(parentText);
     });
 });
