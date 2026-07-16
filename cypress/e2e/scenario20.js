6d555073f5a672e2f9e66fe1e696ece7330f0281
unknown <subhadipdas911@gmail.com>
2026-07-16 17:07:11 +0530

    How to filter out element based on certain values

--- cypress/e2e/scenario20.js
+++ cypress/e2e/scenario20.js
@@ -1,6 +1,24 @@
 /// <reference types='cypress'/>
 
-it("Find and click accordian with button", () => {
+it("Find and click accordian with button : Approach 1", () => {
   cy.visit("/index22.html");
-  cy.contains("div.panel", "I'm here").prev().click();
+  //   cy.get("div.panel:has(button)").prev().click();
+  cy.get("div.panel").each((currEle, index) => {
+    console.log("DEBUG -> ", index);
+    const status = currEle.find("button").length > 0;
+    if (status) {
+      cy.wrap(currEle).prev().click();
+      return false;
+    }
+  });
+});
+
+it("Find and click accordian with button : Approach 2", () => {
+  cy.visit("/index22.html");
+  cy.get("div.panel").find("button").parent().prev().click();
+});
+
+it.only("Find and click accordian with button : Approach 3 [Best Approach]", () => {
+  cy.visit("/index22.html");
+  cy.get("div.panel:has(button)").prev().click();
 });
