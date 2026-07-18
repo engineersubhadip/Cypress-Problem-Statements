1c41975a3825e9e475e94a5870eb513b749253ad
unknown <subhadipdas911@gmail.com>
2026-07-18 11:14:03 +0530

    How to filter out, best approach

--- cypress/e2e/scenario11.js
+++ cypress/e2e/scenario11.js
@@ -1,16 +1,21 @@
 /// <reference types='cypress' />
 
-it("Capture li which has text has Advanced", () => {
-  cy.visit("/index14.html");
-  cy.contains("li", "Advanced!");
+it('Capture li which has text has Advanced', () => {
+	cy.visit('/index14.html');
+	cy.contains('li', 'Advanced!');
 });
 
-it("Capture the top level parents", () => {
-  cy.visit("/index14.html");
-  cy.get("li").find(".label").parents("li");
+it('Capture the top level parents', () => {
+	cy.visit('/index14.html');
+	cy.get('li').find('.label').parents('li');
 });
 
-it.only("Capture li which has warning tags", () => {
-  cy.visit("/index15.html");
-  cy.get("li").find(".badge.warning").parents("li");
+it('Capture li which has warning tags : Filter aproach 1', () => {
+	cy.visit('/index15.html');
+	cy.get('li').find('.badge.warning').parents('li');
+});
+
+it.only('Filter approach 2 : Best approach', () => {
+	cy.visit('/index15.html');
+	cy.get('#list li:has(p)');
 });
